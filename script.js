59cc7f6cded79d97dfbb8222df2df3c6ebf97951
Jana Eickenberg <eickenbergjana@outlook.com>
2024-11-12 10:24:15 -0500

    Added style page and script page

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle Navbar Visibility
+document.getElementById('name').addEventListener('click', () => {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('hidden');
+});
